9b641e1548ee293e2b152b16e491cb8667b7708c
Elliott Fuller <elliottfuller@Elliotts-MacBook-Pro-2.local>
2024-06-06 10:23:36 +1000

    first commit

--- src/main.c
+++ src/main.c
@@ -1,17 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-static char input[2048];
+#include <editline/readline.h>
+
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* Fake readline function */
+char* readline(char* prompt) {
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer)+1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy)-1] = '\0';
+    return cpy;
+}
+
+/* Fake add_history function */
+void add_history(char* unused) {}
+
+/* Otherwise include the editline headers */
+#else
+#include <editline/readline.h>
+#endif
 
 int main(int argc, char** argv) {
     puts("Lispy Version 0.0.0.0.1");
     puts("Press Ctrl+C to Exit\n");
 
     while (1) {
-        fputs("lispy> ", stdout);
+        char* input = readline("lispy> ");
 
-        fgets(input, 2048, stdin);
+        add_history(input);
+        printf("No you're a %s\n", input);
 
-        printf("No you're a %s", input);
+        free(input);
     }
 
     return 0;
